08797f6e55e6cbef6fd035cbe3f5b9e612a8cb00
Semyon <semenb668@gmail.com>
2025-02-21 18:43:21 +0500

    написал json и txt классы

--- file_classes.py
+++ file_classes.py
@@ -1,48 +1,59 @@
 from abc import ABC, abstractmethod
+import json
+import csv
 
 class AbstractFile(ABC):
-    def read():
+    @abstractmethod
+    def read(self):
         '''Абстрактный метод для чтения данных из файла.'''
-        pass
-    def write(data):
+    @abstractmethod
+    def write(self, data):
         '''Абстрактный метод для записи данных в файл'''
-        pass
-    def append(data):
+    @abstractmethod
+    def append(self, data):
         '''Абстрактный метод для добавления данных в файл'''
-        pass
 
 class JsonFile(AbstractFile):
-    file_path = 'data.json'#хз
-    def read():
-        '''Абстрактный метод для чтения данных из файла.'''
-        pass
-    def write(data):
-        '''Абстрактный метод для записи данных в файл'''
-        pass
-    def append(data):
-        '''Абстрактный метод для добавления данных в файл'''
-        pass
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+    def read(self):
+        '''метод для чтения данных из json файла.'''
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    def write(self, data):
+        '''метод для записи данных в json файл'''
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False)
+    def append(self, data):
+        '''метод для добавления данных в json файл'''
+        with open(self.file_path, 'a', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
 class TxtFile(AbstractFile):
-    file_path = 'data.txt'
-    def read():
-        '''Абстрактный метод для чтения данных из файла.'''
-        pass
-    def write(data):
-        '''Абстрактный метод для записи данных в файл'''
-        pass
-    def append(data):
-        '''Абстрактный метод для добавления данных в файл'''
-        pass
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+    def read(self):
+        '''метод для чтения данных из txt файла.'''
+        with open(self.file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    def write(self, data):
+        '''метод для записи данных в txt файл'''
+        with open(self.file_path, 'w', encoding='utf-8') as file:
+            file.write(str(data) + '\n')
+    def append(self, data):
+        '''метод для добавления данных в txt файл'''
+        with open(self.file_path, 'a', encoding="utf-8") as file:
+            file.write(str(data) + '\n')
 
 class CsvFile(AbstractFile):
-    file_path = 'data.csv'
-    def read():
-        '''Абстрактный метод для чтения данных из файла.'''
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+    def read(self):
+        '''метод для чтения данных из csv файла.'''
         pass
-    def write(data):
-        '''Абстрактный метод для записи данных в файл'''
+    def write(self, data):
+        '''метод для записи данных в csv файл'''
         pass
-    def append(data):
-        '''Абстрактный метод для добавления данных в файл'''
+    def append(self, data):
+        '''метод для добавления данных в csv файл'''
         pass
